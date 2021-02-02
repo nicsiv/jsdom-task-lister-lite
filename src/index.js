cf6175ab43cfc0b4b7403b5cd6d8b9c135dac732
nicsiv <nicoleusivek@gmail.com>
2021-02-02 11:05:12 -0600

    final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+let form = document.querySelector("form")
+form.addEventListener("submit", submitTask)
+
+function submitTask(event) {
+  event.preventDefault();
+  let userInput = event.target[0].value;
+  let toDoUl = document.querySelector("#tasks");
+  let newLi = document.createElement("li");
+  newLi.innerText = userInput;
+  toDoUl.append(newLi);
+  event.target[0].value = "";
+}
